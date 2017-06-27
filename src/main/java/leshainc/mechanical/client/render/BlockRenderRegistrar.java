95c1d69410dd390cd33049b0d982ffc19905680c
LeshaInc <includeurl@gmail.com>
2017-06-27 19:56:54 +0300

    Render cactus on top of conveyor belt
    No cactus was hurt.

--- src/main/java/leshainc/mechanical/client/render/BlockRenderRegistrar.java
+++ src/main/java/leshainc/mechanical/client/render/BlockRenderRegistrar.java
@@ -3,14 +3,18 @@ package leshainc.mechanical.client.render;
 import leshainc.mechanical.Mechanical;
 import leshainc.mechanical.common.block.Blocks;
 import leshainc.mechanical.common.block.BlockBeltConveyor;
+import leshainc.mechanical.common.tileentity.TileEntityBeltConveyor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public final class BlockRenderRegistrar {
     public static void registerBlockRenderer() {
         Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
                 .register(Item.getItemFromBlock(Blocks.BELT_CONVEYOR), 0,
                         new ModelResourceLocation(Mechanical.MODID + ":" + BlockBeltConveyor.NAME, "inventory"));
+
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBeltConveyor.class, new RenderBeltConveyor());
     }
 }
---
+++ src/main/java/leshainc/mechanical/client/render/RenderBeltConveyor.java
@@ -0,0 +1,38 @@
+package leshainc.mechanical.client.render;
+
+import leshainc.mechanical.common.tileentity.TileEntityBeltConveyor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+
+public class RenderBeltConveyor extends TileEntitySpecialRenderer {
+    private RenderItem renderItem;
+
+    public RenderBeltConveyor() {
+        this.renderItem = Minecraft.getMinecraft().getRenderItem();
+    }
+
+    @Override
+    public void renderTileEntityAt(TileEntity inTe, double x, double y, double z, float partialTick, int destroyStage) {
+        TileEntityBeltConveyor te = (TileEntityBeltConveyor) inTe;
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
+
+        ItemStack stack = new ItemStack(net.minecraft.init.Blocks.CACTUS);  // TODO: Get ItemStack from TileEntityBeltConveyor
+
+        GlStateManager.translate(0.5, 0.5, 0.5);
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.scale(0.5, 0.5, 0.5);
+
+        renderItem.renderItem(stack, ItemCameraTransforms.TransformType.GROUND);
+
+        GlStateManager.disableRescaleNormal();
+
+        GlStateManager.popMatrix();
+    }
+}
